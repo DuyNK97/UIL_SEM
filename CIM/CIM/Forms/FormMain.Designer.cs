9357f7825c3dd217325684b99004742929c21ef8
ngviet00 <ngvviet0306@gmail.com>
2024-09-24 11:12:03 +0700

    set fullscreen

--- CIM/CIM/Forms/FormMain.Designer.cs
+++ CIM/CIM/Forms/FormMain.Designer.cs
@@ -905,6 +905,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMain";
             this.Text = "CIM UI";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ZoomScaleRect = new System.Drawing.Rectangle(19, 19, 1920, 1055);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox2.ResumeLayout(false);
